4a52f3ef3f4ddae3134d23342e1038f8b85f5c50
aristeidis <aristeidis.kypriotis@gmail.com>
2025-05-30 23:35:31 +0300

    Preparations for GitHub pages deployment #2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace this with your actual repo name
+const repoName = 'snapstats';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${repoName}/`,
+});
